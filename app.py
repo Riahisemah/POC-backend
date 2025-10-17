527005157f9db4c3e4121880da4c3ad6ab9fb067
Riahi Semah <riahisamh817@gmail.com>
2025-10-17 11:41:43 +0100

    test4

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
 
     app = create_app()
 
-    # ⚡ Utilise le port fourni par Railway (variable d'environnement PORT)
+    # Utiliser le port fourni par Railway ou 5000 par défaut
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
